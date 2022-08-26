5c81f9b93d91e182988bbe0282d0ed43f6ab81dd
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-27 01:51:30 +0700

    Update bot-salken.js

--- plugins/bot-salken.js
+++ plugins/bot-salken.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
-	conn.sendButton(m.chat, `Halo Kak👋‹\nsaya adalah Bot WhatsApp yang di bangun menggunakan node.js, Bot ini di edit seseorang yang bernama Rizki Iqbal. Aku Sebuah Bot yang bisa membantumu di grup ini, klik tombol dibawah ini jika kamu ingin menggunakan bot!`.trim(), `${wm}`, [['🌸 Menu', '.m']], m)
+	conn.sendButton(m.chat, `Halo Kak👋‹\nsaya adalah xʜɪᴍᴍᴇʀ ʙᴏᴛ࿐ yang di bangun menggunakan node.js. Aku Sebuah Bot yang bisa membantumu di grup ini, klik tombol dibawah ini jika kamu ingin menggunakan bot!`.trim(), `${wm}`, [['🔥 Menu', '.menu']], m)
 	}
 
 handler.command = /^(salken)$/i
