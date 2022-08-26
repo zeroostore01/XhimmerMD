fb02ff136006511524070491a859ac73172264ee
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-27 02:09:53 +0700

    Update creator3.js

--- plugins/creator3.js
+++ plugins/creator3.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `*⛄ Nyari owner ku mau ngapain?*\n`,wm + '\n\n' + botdate, thumb, [['🦄️BIO DATA','.bioowner'],['🥞HATI NYA','Babi lu']], m, {
+ await conn.sendButton(m.chat, `*⛄ Nyari owner ku mau ngapain?*\n`,wm + '\n\n' + botdate, thumb, [['BIO DATA','.bioowner'],['💞HATI NYA','Babi lu']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: 'DEVELOPER BOT: Mr Iqbal',          previewType: 0,
+                        body: 'DEVELOPER BOT: Vinz',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
                         sourceUrl: snh
                       }}
