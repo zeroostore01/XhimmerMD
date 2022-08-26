e74b47404f7bb638a9724085a21255942ea52896
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-27 03:53:24 +0700

    Update sewa-prem.js

--- plugins/sewa-prem.js
+++ plugins/sewa-prem.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `${htki} PREMIUM ${htka}
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: '💱UPGRADE DI SINI', url: 'https://wa.me/p/5240667609353116'}},
+    {index: 1, urlButton: {displayText: '💱UPGRADE DI SINI', url: 'https://wa.me/6287734910547'}},
     {index: 4, quickReplyButton: {displayText: '🌹SEWA BOT', id: '.belibot'}},
 ]
 let tm = {
