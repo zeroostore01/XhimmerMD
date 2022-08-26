808185837d340875b9b30df465de9e712c45a17b
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-27 02:18:47 +0700

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,25 +1,28 @@
-import { areJidsSameUser } from '@adiwajshing/baileys'
-let handler = async (m, { conn, participants }) => {
-    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
-    let kickedUser = []
-    for (let user of users)
-        if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
-            const res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-            kickedUser.concat(res)
-            await delay(1 * 1000)
-        }
-    m.reply(`Succes kick ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
-
+let handler = async (m, { conn, args }) => {
+let fs = require('fs')
+ let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  aki = m.quoted ? [m.quoted.sender] : m.mentionedJid
+  let users = aki.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
+  wayy = '_*Asik Dapet Jatah Kick*_'
+  for (let i of users) {
+  wayy += ` @${i.split('@')[0]}`
+  }
+  conn.reply(m.chat, wayy, m, { contextInfo: { mentionedJid: users }})
+  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupParticipantsUpdate(m.chat, [user], "remove")
 }
-handler.help = ['kick', '-'].map(v => 'a' + v + ' @user')
-handler.tags = ['admin']
-handler.command = /^(kick|a-)$/i
-
+handler.help = ['kick'].map(v => v + ' @user')
+handler.tags = ['group']
+handler.command = /^(kick|\-)$/i
 handler.owner = false
+handler.mods = false
+handler.premium = false
 handler.group = true
-handler.botAdmin = true
+handler.private = false
+
 handler.admin = true
+handler.botAdmin = true
+
+handler.fail = null
+handler.limit = false
 
 export default handler
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
