db6ec7224fad9c911231b0fd48a94ec9f69dddc7
zeroostore01 <107806866+zeroostore01@users.noreply.github.com>
2022-08-28 14:06:34 +0700

    Update sewa-prem.js

--- plugins/sewa-prem.js
+++ plugins/sewa-prem.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `${htki} PREMIUM ${htka}
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: '💱UPGRADE DI SINI', url: 'https://wa.me/6287734910547'}},
+    {index: 1, urlButton: {displayText: '💱UPGRADE DI SINI', url: 'https://wa.me/6283890585717'}},
     {index: 4, quickReplyButton: {displayText: '🌹SEWA BOT', id: '.belibot'}},
 ]
 let tm = {
